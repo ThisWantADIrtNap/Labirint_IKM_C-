07c3c01b4c6b78e0b06f45a9774b5dd99846c41a
GDDR_5 <dimasikstk@gmail.com>
2026-01-21 01:17:56 +0500

    finish h.h

--- h.h
+++ h.h
@@ -1,23 +1,25 @@
 #ifndef H_H
 #define H_H
 
+#include <iostream>
 #include <vector>
 #include <string>
 
-// структура для хранения координат
+// Структуры
 struct Point {
     int x, y;
 };
-// фулл структура лабиринта (размеры, символы, пройденный путь, волновой алгоритм для поиска пути, старт и финиш)
+
 struct MazeData {
-    int width, height; 
+    int width;
+    int height;
     std::vector<std::vector<char>> grid;
     std::vector<std::vector<bool>> visited;
     std::vector<std::vector<int>> dist;
     Point start;
     Point finish;
 };
-// структура интерфейса(лабиринт, игрок, передвежения, мин. путь)
+
 struct GameState {
     MazeData* maze;
     Point player;
@@ -25,9 +27,9 @@ struct GameState {
     int shortestPath;
 };
 
-// функции для работы с лабиринтом
+// Функции для работы с лабиринтом
 MazeData* createMaze(int width, int height);
-void generateMaze(MazeData* maze);
+void generateMaze(MazeData* maze);  // Простая генерация без стека
 void setStartAndFinish(MazeData* maze);
 int findShortestPath(MazeData* maze);
 void displayMaze(const MazeData* maze, Point player);
@@ -37,11 +39,11 @@ bool saveMazeToFile(const MazeData* maze, const std::string& filename, Point pla
 MazeData* loadMazeFromFile(const std::string& filename, Point& player, int& moves);
 void freeMaze(MazeData* maze);
 
-// функции для игры
+// Функции для игры
 GameState* createGame(int width, int height);
 GameState* loadGameFromMaze(MazeData* maze, Point player, int moves);
 bool saveGame(const GameState* game, const std::string& filename = "save.txt");
 void playGame(GameState* game);
 void freeGame(GameState* game);
 
-#endif
+#endif 
